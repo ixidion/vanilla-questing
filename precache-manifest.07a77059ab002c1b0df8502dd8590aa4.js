0d492b1dd8dff27d7bf54db3d3f0bccee811d8a4
ixidion <ixidion@bluemx.de>
2019-08-01 23:07:47 +0200

    Updates

--- precache-manifest.07a77059ab002c1b0df8502dd8590aa4.js
+++ precache-manifest.07a77059ab002c1b0df8502dd8590aa4.js
@@ -1,23 +1,19 @@
-self.__precacheManifest = (self.__precacheManifest || []).concat([
+self.__precacheManifest = [
   {
-    "revision": "8f9794f541ad06400586c1de42a99a27",
-    "url": "/index.html"
+    "revision": "cea2c0154c8215629112",
+    "url": "/static/css/main.d0986327.chunk.css"
   },
   {
-    "revision": "cd9f5cbdf6d865344ce2",
-    "url": "/static/css/main.0e6d5de8.chunk.css"
+    "revision": "cea2c0154c8215629112",
+    "url": "/static/js/main.cea2c015.chunk.js"
   },
   {
-    "revision": "8b29b6c4af1d191c8eff",
-    "url": "/static/js/2.1b498035.chunk.js"
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
   },
   {
-    "revision": "cd9f5cbdf6d865344ce2",
-    "url": "/static/js/main.3c921523.chunk.js"
-  },
-  {
-    "revision": "42ac5946195a7306e2a5",
-    "url": "/static/js/runtime~main.a8a9905a.js"
+    "revision": "5ddf4703f145fe2d9215",
+    "url": "/static/js/2.5ddf4703.chunk.js"
   },
   {
     "revision": "1f036d8118bd47c5e62fc560cbe0d572",
@@ -42,10 +38,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "e1cc9f52dc2df22471b2c11941eb8b1d",
     "url": "/static/media/barrens.e1cc9f52.jpg"
-  },
-  {
-    "revision": "cea427b3bdaa25a49f9f6ab7b195ad1c",
-    "url": "/static/media/bg.cea427b3.png"
   },
   {
     "revision": "f1e16f66ea5c1155e1929e5d73c95eda",
@@ -110,10 +102,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "7febd91b3e060d5b6b3f27989b1fa5c0",
     "url": "/static/media/loch.7febd91b.jpg"
-  },
-  {
-    "revision": "3eb14f3838ada50e10f062a895c3b9cf",
-    "url": "/static/media/main.3eb14f38.woff"
   },
   {
     "revision": "0e8a36949d7771bb32438ec3e845fdb1",
@@ -206,5 +194,17 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "1676a0cce28972f08a2b519875cf0f04",
     "url": "/static/media/wpl.1676a0cc.jpg"
+  },
+  {
+    "revision": "cea427b3bdaa25a49f9f6ab7b195ad1c",
+    "url": "/static/media/bg.cea427b3.png"
+  },
+  {
+    "revision": "3eb14f3838ada50e10f062a895c3b9cf",
+    "url": "/static/media/main.3eb14f38.woff"
+  },
+  {
+    "revision": "cdcc7f904beb1911be0ad2dfe5d56fdf",
+    "url": "/index.html"
   }
-]);
+];
